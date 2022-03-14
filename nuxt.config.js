aefa533368386b1cebb23e068a77b7793ea94552
fahmisyaifudin <fahmisyaifudin00@gmail.com>
2022-03-14 07:12:33 +0700

    add env variable

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,5 +62,14 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  env: {
+    FB_API_KEY: process.env.FB_API_KEY,
+    FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
+    FB_PROJECT_ID: process.env.FB_PROJECT_ID,
+    FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET,
+    FB_MESSAGE_SENDER_ID: process.env.FB_MESSAGE_SENDER_ID,
+    FB_APP_ID: process.env.FB_APP_ID
   }
 }
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,8 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import firebaseConfig from "../firebase-config.json";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+const firebaseConfig = {
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    projectId: process.env.FB_PROJECT_ID,
+    storageBucket: process.env.FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.FB_MESSAGE_SENDER_ID,
+    appId: process.env.FB_APP_ID
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
