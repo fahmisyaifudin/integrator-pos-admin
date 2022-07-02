e411c307e5977330b6917b5709626e6f9f332964
fahmisyaifudin <fahmisyaifudin00@gmail.com>
2022-07-02 16:21:00 +0700

    add filter and top trend product

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
               <i class="material-icons opacity-10">weekend</i>
             </div>
             <div class="text-end pt-1">
-              <p class="text-sm mb-0 text-capitalize">This Month Omzet</p>
+              <p class="text-sm mb-0 text-capitalize">Omzet</p>
               <h4 class="mb-0">{{ $convertToRupiah(totalEarn) }}</h4>
             </div>
           </div>
@@ -25,7 +25,7 @@
               <i class="material-icons opacity-10">person</i>
             </div>
             <div class="text-end pt-1">
-              <p class="text-sm mb-0 text-capitalize">This Month Transaction</p>
+              <p class="text-sm mb-0 text-capitalize">Transaction</p>
               <h4 class="mb-0">{{ totalTrx }}</h4>
             </div>
           </div>
@@ -40,18 +40,49 @@
     <div class="col-12">
       <div class="card">
         <div class="card-header">
-          <h5> Trend Sales on This Month</h5>
+          <div class="row">
+            <div class="col-md-6">
+              <h5> Trend Sales</h5>
+            </div>
+            <div class="col-md-3">
+                <label>Start Date</label>
+                <div class="input-group input-group-outline">
+                  <input type="date" class="form-control" v-model="filter.startDate" @change="() => {
+                    updateChart(); updateSummary(); updateTopProduct();
+                  }">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <label>End Date</label>
+                <div class="input-group input-group-outline">
+                  <input type="date" class="form-control" v-model="filter.endDate" @change="() => {
+                    updateChart(); updateSummary(); updateTopProduct();
+                  }">
+                </div>
+            </div>
+          </div>
         </div>
         <div class="card-body">
             <client-only>
-              <div>
-                <VueApexCharts
-                  ref="demoChart"
-                  height="400"
-                  type="area"
-                  :options="chartOptions"
-                  :series="series"
-                ></VueApexCharts>
+              <div class="row">
+                <div class="col-md-8">
+                  <VueApexCharts
+                    ref="areaChart"
+                    height="400"
+                    type="area"
+                    :options="lineChartOptions"
+                    :series="lineSeries"
+                  ></VueApexCharts>
+                </div>
+                <div class="col-md-4">
+                  <VueApexCharts
+                    ref="barChart"
+                    height="400"
+                    type="bar"
+                    :options="barChartOptions"
+                    :series="barSeries"
+                  ></VueApexCharts>
+                </div>
               </div>
             </client-only>
         </div>
@@ -62,6 +93,7 @@
 </template>
 
 <script>
+import moment from "moment";
 
 export default {
     layout: 'admin',
@@ -72,7 +104,11 @@ export default {
       return {
         totalEarn: 0,
         totalTrx: 0,
-        chartOptions: {
+        filter: {
+          startDate: moment().subtract(30, 'days').format('YYYY-MM-DD'),
+          endDate: moment().format('YYYY-MM-DD')
+        },
+        lineChartOptions: {
           chart: {
             id: 'vuechart-example',
             toolbar: {
@@ -88,12 +124,36 @@ export default {
             }
           }
         },
-        series: [
+        barChartOptions: {
+          chart: {
+            type: 'bar',
+            height: 350,
+            toolbar: {
+              show: false
+            }
+          },
+          plotOptions: {
+            bar: {
+              borderRadius: 4,
+              horizontal: true,
+            }
+          },
+          dataLabels: {
+            enabled: false
+          },
+          xaxis: {
+            categories: [],
+          }
+        },
+        lineSeries: [
           {
             name: 'Total Omzet',
             data: []
           }
-        ]
+        ],
+        barSeries:  [{
+            data: []
+        }],
       }
     },
     beforeCreate(){
@@ -101,25 +161,48 @@ export default {
       this.$axios.setToken(this.$store.state.auth.accessToken, 'Bearer')
     },
     created(){
-        this.$axios.get('/api/dashboard/summary').then(res => {
-          this.totalEarn = res.data.data.nominal ? res.data.data.nominal : 0
-          this.totalTrx = res.data.data.count
-        })
-
-        this.$axios.get('/api/dashboard/graph').then(res => {
+        this.updateChart()
+        this.updateSummary()
+        this.updateTopProduct()
+       
+    },
+    mounted(){
+      
+    },
+    methods: {
+      updateChart(){
+         this.$axios.get(`/api/dashboard/graph?start=${this.filter.startDate}&end=${this.filter.endDate}`).then(res => {
           let data = res.data.data.map(val => {
             return {
               x: val.day,
               y: val.sum
             }
           })
-          this.$refs.demoChart.updateSeries([{
+          
+          this.$refs.areaChart.updateSeries([{
             data: data
           }])
         })
-    },
-    mounted(){
-      
+      },
+      updateSummary(){
+        this.$axios.get(`/api/dashboard/summary?start=${this.filter.startDate}&end=${this.filter.endDate}`).then(res => {
+          this.totalEarn = res.data.data.nominal ? res.data.data.nominal : 0
+          this.totalTrx = res.data.data.count
+        })
+      },
+      updateTopProduct(){
+         this.$axios.get(`/api/dashboard/top-product?start=${this.filter.startDate}&end=${this.filter.endDate}`).then(res => {
+          let data = res.data.data.map(val => {
+            return {
+              x: val.name,
+              y: val.count
+            }
+          })
+          this.$refs.barChart.updateSeries([{
+            data: data
+          }])
+        })
+      }
     }
 }
 </script>
